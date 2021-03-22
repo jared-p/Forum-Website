ec0eaf35eabfc1cb7d4d001589fce1dbb908ee4c
Zmontgomery96 <Zacharymontgomery1996@gmail.com>
2021-03-22 01:44:33 -0700

    initial outline for account page

---
+++ userAccount.php
@@ -0,0 +1,37 @@
+<?php
+require 'include/db_credentials.php';
+?>
+<!DOCTYPE html>
+
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <title>Main Page</title>
+    <link rel="stylesheet" href="css/main.css">
+    <script src="https://code.jquery.com/jquery-3.1.1.min.js"></script>
+    <script type="text/javascript">
+        window.jQuery || document.write('<script src=“js/jquery-3.1.1.min.js”><\/script>');
+    </script>
+    <script type="text/javascript" src="js/main.js"></script>
+
+</head>
+    <body>
+        <?php include 'header.php'; ?>
+    
+        <!-- included all teh stuff above to make the doc work -->
+        <!-- going to have the same style as main with the same header (reddit does this) -->
+
+        <!-- have user info with email etc -->
+        <!-- could have the profile with all the user info on the left box to follow main css look -->
+
+        <!-- display current user logged in -->
+        <!-- change login button to logout -->
+
+
+        <!-- reddit uses a secondary header, maybe integrate that -->
+        <!-- tab for posts made by user -->
+        <!-- maybe see where they commented -->
+
+    </body>
+</html>
