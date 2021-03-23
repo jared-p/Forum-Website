fdc4eaa68578375292fea0b63bd52f184bc3b06f
Zmontgomery96 <Zacharymontgomery1996@gmail.com>
2021-03-22 21:38:25 -0700

    added rough code, queries are next

--- userAccount.php
+++ userAccount.php
@@ -17,11 +17,43 @@ require 'include/db_credentials.php';
 
 </head>
     <body>
-        <?php include 'header.php'; ?>
-    
+        <?php 
+        include 'header.php'; 
+        ?>
+    <div class = "main">
+        <!-- have to link to this after checking session variable user and seeing if they are logged in -->
+
         <!-- included all teh stuff above to make the doc work -->
         <!-- going to have the same style as main with the same header (reddit does this) -->
 
+        <div class = "left_col">
+            <?php    
+            // $user = $_SESSION["user"];
+            $user = "this guy";
+            $username = $_SESSION['username'];
+            $test = $_GET['user'];
+            print_r($_SESSION);
+            ?>
+            <?php if(empty($user)){
+                echo "thing done";
+            }elseif(empty($test)){
+                echo "thing not done";
+            }else{
+                echo "damn dude";
+            }
+            ?>
+            <h1 class="left_col_title"><?php echo $user;?></h1>
+        </div>
+        
+        <div class = "right_col">
+            <div class = "post">
+            <h1 class = "right_col_title">Title of the posts they have made</h1>
+            <p class = "post_content">some kind of content</p>
+            <p class = "post_information">query info relative to user</p>
+            </div>
+        </div>
+
+    </div>
         <!-- have user info with email etc -->
         <!-- could have the profile with all the user info on the left box to follow main css look -->
 
