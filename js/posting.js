be10d6924aaa13fe3945cbd4d3a0c03afc299d46
Jrock <60457530+jared-p@users.noreply.github.com>
2021-03-25 01:28:29 -0700

    fixed comments AJAX

--- js/posting.js
+++ js/posting.js
@@ -1,51 +1,67 @@
 window.addEventListener('load', function () {
     //will continiously call the function to show the comments
-    //setInterval(function(e){
-    var postid = $(".post").attr("id");
-    var url = "getComments.php";
-    $.post(url, { 'postid': postid }, function (data, status) {
-        if (status == 'success') {
-            var comments = $.parseJSON(data);
-            if (comments.length != 0) {
-                $("#comments").before($('<h3 id="comments_title">Comments: ' + comments.length + '</h3>'));
-                for (var i = 0; i < comments.length; i++) {
-                    var numComments = 0;
-                    for (var j = 0; j < comments.length; j++) {
-                        if (comments[j].parentid == comments[i].commentid) {
-                            numComments++;
+    setInterval(function (e) {
+        var postid = $(".post").attr("id");
+        var url = "getComments.php";
+        $.post(url, { 'postid': postid }, function (data, status) {
+            if (status == 'success') {
+                var comments = $.parseJSON(data);
+                if (comments.length != 0) {
+                    for (var i = 0; i < comments.length; i++) {
+                        var numComments = 0;
+                        for (var j = 0; j < comments.length; j++) {
+                            if (comments[j].parentid == comments[i].commentid) {
+                                numComments++;
+                            }
                         }
-                    }
-                    if (comments[i].parentid == null) {
-                        var node = $("<div id='c" + comments[i].commentid + "' class='root_comment'>" + comments[i].body + "</div>");
-                        node.on("click", collapseComment);
-                        var commentNode = $("<p class='left_align'>" + numComments + " Replies</p>");
-                        var infoNode = $('<p class="comment_information">Username: ' + comments[i].username + ', Posted on: ' + comments[i].commentdate + '</p>');
-                        node.append(infoNode);
-                        node.append(commentNode);
-                        $("#comments").prepend(node);
-                    } else {
-                        var node = $("<div id='c" + comments[i].commentid + "' class='sub_comment hide'>" + comments[i].body + "</div>");
-                        node.on("click", collapseComment);
-                        var commentNode = $("<p class='left_align'>" + numComments + " Replies</p>");
-                        var infoNode = $('<p class="comment_information">Username: ' + comments[i].username + ', Posted on: ' + comments[i].commentdate + '</p>');
-                        node.append(infoNode);
-                        node.append(commentNode);
-                        $('#c' + comments[i].parentid).append(node);
-                    }
+                        if ($("#comments_title").length == 0) {
+                            $("#comments").before($('<h3 id="comments_title">Comments: ' + comments.length + '</h3>'));
+                        } else {
+                            $("#comments_title").text('Comments: ' + comments.length);
+                            $('#no_comments').remove();
+                        }
+                        if (comments[i].parentid == null) {
+                            var testnode = $('#c' + comments[i].commentid);
+                            if (testnode.length == 0) {
+                                var node = $("<div id='c" + comments[i].commentid + "' class='root_comment'>" + comments[i].body + "</div>");
+                                node.on("click", collapseComment);
+                                var commentNode = $("<p class='left_align' id='" + postid + comments[i].commentid + "'>" + numComments + " Replies</p>");
+                                var infoNode = $('<p class="comment_information">Username: ' + comments[i].username + ', Posted on: ' + comments[i].commentdate + '</p>');
+                                node.append(infoNode);
+                                node.append(commentNode);
+                                $("#comments").prepend(node);
+                            } else {
+                                $('#' + postid + comments[i].commentid).text(numComments + " Replies");
+                            }
+                        } else {
+                            var testnode = $('#c' + comments[i].commentid);
+                            //console.log(testnode.length == 0);
+                            if (testnode.length == 0) {
+                                var node = $("<div id='c" + comments[i].commentid + "' class='sub_comment hide'>" + comments[i].body + "</div>");
+                                node.on("click", collapseComment);
+                                var commentNode = $("<p class='left_align'>" + numComments + " Replies</p>");
+                                var infoNode = $('<p class="comment_information">Username: ' + comments[i].username + ', Posted on: ' + comments[i].commentdate + '</p>');
+                                node.append(infoNode);
+                                node.append(commentNode);
+                                $('#c' + comments[i].parentid).append(node);
+                            }
+                        }
 
-                    //console.log(comments[i]);
+                        //console.log(comments[i]);
 
+                    }
+                } else {
+                    if ($("#comments_title").length == 0) {
+                        $("#comments").before($('<h3 id="comments_title">Comments: ' + comments.length + '</h3>'));
+                        $("#comments").append($("<p id='no_comments'>No Comments Posted Yet!</p>"));
+                    }
                 }
             } else {
-                $("#comments").prepend($('<h3 id="comments_title">Comments</h3>'));
-                $("#comments").append($("<p id='no_comments'>No Comments Posted Yet!</p>"));
+                alert("error");
             }
-        } else {
-            alert("error");
-        }
-        //console.log($("#comments")[0].children.length);
-    });
-    //}, 1000);
+            //console.log($("#comments")[0].children.length);
+        });
+    }, 1000);
 });
 function collapseComment(e) {
     e.stopPropagation();
--- js/user_posting.js
+++ js/user_posting.js
@@ -1,75 +1,99 @@
 window.addEventListener('load', function () {
     //will continiously call the function to show the comments
-    //setInterval(function(e){
-    var postid = $(".post").attr("id");
-    var url = "getComments.php";
-    $.post(url, { 'postid': postid }, function (data, status) {
-        if (status == 'success') {
-            var comments = $.parseJSON(data);
-            if (comments.length != 0) {
-                $("#comments").before($('<h3 id="comments_title">Comments: ' + comments.length + '</h3>'));
-                for (var i = 0; i < comments.length; i++) {
-                    var numComments = 0;
-                    for (var j = 0; j < comments.length; j++) {
-                        if (comments[j].parentid == comments[i].commentid) {
-                            numComments++;
+    setInterval(function (e) {
+        var postid = $(".post").attr("id");
+        var url = "getComments.php";
+        $.post(url, { 'postid': postid }, function (data, status) {
+            if (status == 'success') {
+                var comments = $.parseJSON(data);
+                if (comments.length != 0) {
+                    for (var i = 0; i < comments.length; i++) {
+                        var numComments = 0;
+                        for (var j = 0; j < comments.length; j++) {
+                            if (comments[j].parentid == comments[i].commentid) {
+                                numComments++;
+                            }
                         }
+                        if ($("#comments_title").length == 0) {
+                            $("#comments").before($('<h3 id="comments_title">Comments: ' + comments.length + '</h3>'));
+                            var formNode = $('<form action="createComment.php" method="post"></form>');
+                            var createButton = $('<input type="submit" value="Add Comment">');
+                            var hiddenInput = $('<input type="hidden" name="commentid" value="' + null + '">');
+                            var hidden_postid = $('<input type="hidden" name="postid" value="' + postid + '">');
+                            formNode.append(createButton);
+                            formNode.append(hiddenInput);
+                            formNode.append(hidden_postid);
+                            $("#comments").before(formNode);
+                        } else {
+                            $("#comments_title").text('Comments: ' + comments.length);
+                            $('#no_comments').remove();
+                        }
+                        if (comments[i].parentid == null) {
+                            var testnode = $('#c' + comments[i].commentid);
+                            if (testnode.length == 0) {
+                                var node = $("<div id='c" + comments[i].commentid + "' class='root_comment'>" + comments[i].body + "</div>");
+                                node.on("click", collapseComment);
+                                var commentNode = $("<p class='left_align' id='" + postid + comments[i].commentid + "'>" + numComments + " Replies</p>");
+                                var infoNode = $('<p class="comment_information">Username: ' + comments[i].username + ', Posted on: ' + comments[i].commentdate + '</p>');
+                                var formNode = $('<form action="createComment.php" method="post" class="hide"></form>');
+                                var editButton = $('<input type="submit" value="Add Comment">');
+                                var hiddenInput = $('<input type="hidden" name="commentid" value="' + comments[i].commentid + '">');
+                                var hidden_postid = $('<input type="hidden" name="postid" value="' + postid + '">');
+                                formNode.append(editButton);
+                                formNode.append(hiddenInput);
+                                formNode.append(hidden_postid);
+                                node.append(formNode);
+                                node.append(infoNode);
+                                node.append(commentNode);
+                                $("#comments").prepend(node);
+                            } else {
+                                $('#' + postid + comments[i].commentid).text(numComments + " Replies");
+                            }
+                        } else {
+                            var testnode = $('#c' + comments[i].commentid);
+                            //console.log(testnode.length == 0);
+                            if (testnode.length == 0) {
+                                var node = $("<div id='c" + comments[i].commentid + "' class='sub_comment hide'>" + comments[i].body + "</div>");
+                                node.on("click", collapseComment);
+                                var commentNode = $("<p class='left_align'>" + numComments + " Replies</p>");
+                                var infoNode = $('<p class="comment_information">Username: ' + comments[i].username + ', Posted on: ' + comments[i].commentdate + '</p>');
+                                var formNode = $('<form action="createComment.php" method="post" class="hide"></form>');
+                                var editButton = $('<input type="submit" value="Add Comment">');
+                                var hiddenInput = $('<input type="hidden" name="commentid" value="' + comments[i].commentid + '">');
+                                var hidden_postid = $('<input type="hidden" name="postid" value="' + postid + '">');
+                                formNode.append(editButton);
+                                formNode.append(hiddenInput);
+                                formNode.append(hidden_postid);
+                                node.append(formNode);
+                                node.append(infoNode);
+                                node.append(commentNode);
+                                $('#c' + comments[i].parentid).append(node);
+                            }
+                        }
+
+                        //console.log(comments[i]);
+
                     }
-                    if (comments[i].parentid == null) {
-                        var node = $("<div id='c" + comments[i].commentid + "' class='root_comment'>" + comments[i].body + "</div>");
-                        node.on("click", collapseComment);
-                        var commentNode = $("<p class='left_align'>" + numComments + " Replies</p>");
-                        var infoNode = $('<p class="comment_information">Username: ' + comments[i].username + ', Posted on: ' + comments[i].commentdate + '</p>');
-                        var formNode = $('<form action="createComment.php" method="post" class="hide"></form>');
-                        var editButton = $('<input type="submit" value="Add Comment">');
-                        var hiddenInput = $('<input type="hidden" name="commentid" value="' + comments[i].commentid + '">');
+                } else {
+                    if ($("#comments_title").length == 0) {
+                        $("#comments").before($('<h3 id="comments_title">Comments: ' + comments.length + '</h3>'));
+                        var formNode = $('<form action="createComment.php" method="post"></form>');
+                        var createButton = $('<input type="submit" value="Add Comment">');
+                        var hiddenInput = $('<input type="hidden" name="commentid" value="' + null + '">');
                         var hidden_postid = $('<input type="hidden" name="postid" value="' + postid + '">');
-                        formNode.append(editButton);
+                        formNode.append(createButton);
                         formNode.append(hiddenInput);
                         formNode.append(hidden_postid);
-                        node.append(formNode);
-                        node.append(infoNode);
-                        node.append(commentNode);
-                        $("#comments").prepend(node);
-                    } else {
-                        var node = $("<div id='c" + comments[i].commentid + "' class='sub_comment hide'>" + comments[i].body + "</div>");
-                        node.on("click", collapseComment);
-                        var commentNode = $("<p class='left_align'>" + numComments + " Replies</p>");
-                        var infoNode = $('<p class="comment_information">Username: ' + comments[i].username + ', Posted on: ' + comments[i].commentdate + '</p>');
-                        var formNode = $('<form action="createComment.php" method="post" class="hide"></form>');
-                        var editButton = $('<input type="submit" value="Add Comment">');
-                        var hiddenInput = $('<input type="hidden" name="commentid" value="' + comments[i].commentid + '">');
-                        var hidden_postid = $('<input type="hidden" name="postid" value="' + postid + '">');
-                        formNode.append(editButton);
-                        formNode.append(hiddenInput);
-                        formNode.append(hidden_postid);
-                        node.append(formNode);
-                        node.append(infoNode);
-                        node.append(commentNode);
-                        $('#c' + comments[i].parentid).append(node);
+                        $("#comments").before(formNode);
+                        $("#comments").append($("<p id='no_comments'>No Comments Posted Yet!</p>"));
                     }
-
-                    //console.log(comments[i]);
-
                 }
             } else {
-                $("#comments").prepend($('<h3 id="comments_title">Comments</h3>'));
-                $("#comments").append($("<p id='no_comments'>No Comments Posted Yet!</p>"));
+                alert("error");
             }
-        } else {
-            alert("error");
-        }
-        //console.log($("#comments")[0].children.length);
-    });
-    var formNode = $('<form action="createComment.php" method="post"></form>');
-    var createButton = $('<input type="submit" value="Add Comment">');
-    var hiddenInput = $('<input type="hidden" name="commentid" value="' + null + '">');
-    var hidden_postid = $('<input type="hidden" name="postid" value="' + postid + '">');
-    formNode.append(createButton);
-    formNode.append(hiddenInput);
-    formNode.append(hidden_postid);
-    $("#comments").before(formNode);
-    //}, 1000);
+            //console.log($("#comments")[0].children.length);
+        });
+    }, 1000);
 });
 function collapseComment(e) {
     e.stopPropagation();
