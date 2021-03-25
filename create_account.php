57dcade698ba010c1f96104b7b40611a19b07453
Jrock <60457530+jared-p@users.noreply.github.com>
2021-03-25 03:50:41 -0700

    allow user to create account with image

--- create_account.php
+++ create_account.php
@@ -19,7 +19,7 @@ require 'include/db_credentials.php';
 <body>
   <?php include 'header.php'; ?>
   <div id="main_wrapper">
-    <form method="post" action="create_account.php" id="user_form">
+    <form method="post" action="create_account.php" id="user_form" enctype="multipart/form-data">
       <fieldset>
         <legend>
           <h1 id="header">Create Account</h1>
@@ -47,32 +47,46 @@ require 'include/db_credentials.php';
           All fields must be entered
         </div>
 
-        <input type="submit" />
+        <input type="submit" value="Create Account" />
         <input type="reset" />
-
-        <?php
-
-        $id = $_POST["id"];
-        $email = $_POST["email"];
-        $password = $_POST["password"];
-        $fname = $_POST["fname"];
-        $lname = $_POST["lname"];
-        $userImage = file_get_contents($_POST["photo"]);
-
-        $sqlinsert = "INSERT INTO users(username, pass, firstName, lastName, email, pic) VALUES (?,?,?,?,?,?)";
-        $statement = $pdo->prepare($sqlinsert);
-        $statement->bindValue(1, $id);
-        $statement->bindValue(2, $password);
-        $statement->bindValue(3, $fname);
-        $statement->bindValue(4, $lname);
-        $statement->bindValue(5, $email);
-        $statement->bindParam(6, $userImage, PDO::PARAM_LOB);
-        $statement->execute();
-        ?>
-
       </fieldset>
     </form>
-    <a href="main.php">Go to Main Page</a>
+    <?php
+    $id = "";
+    $email = "";
+    $password = "";
+    $fname = "";
+    $lname = "";
+    $userImage = "";
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+      $id = $_POST["id"] ?? "";
+      $email = $_POST["email"] ?? "";
+      $password = $_POST["password"] ?? "";
+      $fname = $_POST["fname"] ?? "";
+      $lname = $_POST["lname"] ?? "";
+      $userImage = $_FILES["photo"]["name"] ?? "";
+      if ($userImage != "") {
+        if ($_FILES["photo"]["size"] <= 64000) {
+          if ($id != "" && $email != "" && $password != "" && $fname != "" && $lname != "" && $userImage != "") {
+            $image = file_get_contents($_FILES["photo"]['tmp_name']);
+            $sqlinsert = "INSERT INTO users(username, pass, firstName, lastName, email, pic) VALUES (?,?,?,?,?,?)";
+            $statement = $pdo->prepare($sqlinsert);
+            $statement->bindValue(1, $id);
+            $statement->bindValue(2, $password);
+            $statement->bindValue(3, $fname);
+            $statement->bindValue(4, $lname);
+            $statement->bindValue(5, $email);
+            $statement->bindParam(6, $image, PDO::PARAM_LOB);
+            $statement->execute();
+            echo "<a href='login.php'>Account successfully created! Click here to login</a>";
+          }
+        } else {
+          echo "<p>Upload Failed: Uploaded image file must be under 64 KB</p>";
+        }
+      }
+    }
+    ?>
+    <!-- <a href="main.php">Go to Main Page</a> -->
   </div>
 </body>
 
